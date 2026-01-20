08d4f83674a49e1c0e5647d412344357329fa0c9
Tyson-Zwicker <tysonzwicker@gmail.com>
2026-01-19 21:26:43 -0400

    more info..

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import HexGrid from './hexgrid.js';
 
-const grid = new HexGrid(30, 'flat'); // 30px hexes, flat orientation
+const grid = new HexGrid(20, 'flat'); // 30px hexes, flat orientation
 const neighbors = grid.getNeighborsInRadius(0, 0, 2);
 const distance = grid.getDistance(0, 0, 3, 2);
 const direction = grid.getDirection(0, 0, 3, 2);
@@ -8,11 +8,15 @@ const hex = grid.screenToGrid(150, 200);
 document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 document.addEventListener('DOMContentLoaded', function () {
-  grid.drawGrid(ctx, 0, 30, 0, 30, 15, 15, canvas.width, canvas.height,'#112255', '#557700' , 2);
+  grid.drawGrid(ctx, 0, 10, 0, 10, 5, 5, canvas.width, canvas.height,'#112255', '#557700' , 2);
+  console.log (grid.hexToPixel (0,0));
+    console.log (grid.hexToPixel (10,0));
+    console.log (grid.hexToPixel (10,10));
+    console.log (grid.hexToPixel (0,10));
   canvas.addEventListener ('mouseup',(e)=>{
     let coord = grid.screenToGrid (e.clientX, e.clientY);
     console.log (coord);
     let coords2 = grid.hexToPixel(coord.q, coord.r);
-    console.log (coords2);
+    console.log (coords2);    
   });
 });
